f6007d38a2783a317cc09eebba6f4fdf557ffd57
laolarou <luolinrui726@outlook.com>
2025-03-01 20:50:09 -0800

    Update TaskHelper.cs

--- ConnectX.Shared/Helpers/TaskHelper.cs
+++ ConnectX.Shared/Helpers/TaskHelper.cs
@@ -6,18 +6,34 @@ public static class TaskHelper
 {
     public static void Forget(this Task t)
     {
+        t.ContinueWith(r =>
+        {
+            Debug.WriteLine(r.Exception);
+        }, TaskContinuationOptions.OnlyOnFaulted);
     }
 
     public static void Forget<T>(this Task<T> t)
     {
+        t.ContinueWith(r =>
+        {
+            Debug.WriteLine(r.Exception);
+        }, TaskContinuationOptions.OnlyOnFaulted);
     }
 
     public static void Forget(this ValueTask t)
     {
+        t.AsTask().ContinueWith(r =>
+        {
+            Debug.WriteLine(r.Exception);
+        }, TaskContinuationOptions.OnlyOnFaulted);
     }
 
     public static void Forget<T>(this ValueTask<T> t)
     {
+        t.AsTask().ContinueWith(r =>
+        {
+            Debug.WriteLine(r.Exception);
+        }, TaskContinuationOptions.OnlyOnFaulted);
     }
 
     public static async ValueTask WaitUntilAsync(Func<bool> predicate, CancellationToken cancellationToken = default)
