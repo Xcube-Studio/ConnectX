8e0755a4fd2effc0f79644fe2a8a694bcf6acf4b
laolarou <luolinrui726@outlook.com>
2025-03-30 22:20:44 -0700

    性能优化、项目升级
    启用新版解决方案文件 slnx
    为封包注册启用源生成器

---
+++ ConnectX.MessageRegister.SourceGenerator/Main.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace ConnectX.MessageRegister.SourceGenerator;
+
+[Generator]
+public class PacketRegisterSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var classesWithAttribute = context.SyntaxProvider
+            .CreateSyntaxProvider(IsCandidate, Transform)
+            .Where(static m => m is not null);
+
+        var compilationAndClasses = context.CompilationProvider.Combine(classesWithAttribute.Collect());
+
+        context.RegisterSourceOutput(compilationAndClasses, (spc, source) =>
+        {
+            var (compilation, classes) = source;
+
+            var packetTypes = classes
+                .Select(typeSymbol => typeSymbol.ToDisplayString())
+                .ToList();
+
+            var assemblyName = compilation.AssemblyName ?? "Generated";
+            var generated = SourceGenHelper.GetCompleteDecl(packetTypes, assemblyName);
+
+            var codeString = generated.NormalizeWhitespace().ToFullString();
+            spc.AddSource("PacketRegisterHelper.cs", SourceText.From(codeString, Encoding.UTF8));
+        });
+    }
+
+    private static bool IsCandidate(SyntaxNode node, CancellationToken _) =>
+        node is ClassDeclarationSyntax or StructDeclarationSyntax;
+
+    private static INamedTypeSymbol? Transform(GeneratorSyntaxContext context, CancellationToken _)
+    {
+        var typeDeclaration = (TypeDeclarationSyntax)context.Node;
+
+        foreach (var attributeList in typeDeclaration.AttributeLists)
+        {
+            foreach (var attribute in attributeList.Attributes)
+            {
+                if (context.SemanticModel.GetSymbolInfo(attribute).Symbol is IMethodSymbol attributeSymbol)
+                {
+                    var attributeContainingType = attributeSymbol.ContainingType;
+                    if (attributeContainingType.ToDisplayString() == "Hive.Codec.Shared.MessageDefineAttribute")
+                    {
+                        return context.SemanticModel.GetDeclaredSymbol(typeDeclaration);
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ ConnectX.MessageRegister.SourceGenerator/SourceGenHelper.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace ConnectX.MessageRegister.SourceGenerator;
+
+public static class SourceGenHelper
+{
+    private static SyntaxList<UsingDirectiveSyntax> GetUsings()
+    {
+        return List(
+        [
+            UsingDirective(
+                QualifiedName(
+                    QualifiedName(
+                        IdentifierName("Microsoft"),
+                        IdentifierName("Extensions")),
+                    IdentifierName("DependencyInjection"))),
+            UsingDirective(
+                QualifiedName(
+                    QualifiedName(
+                        IdentifierName("Hive"),
+                        IdentifierName("Codec")),
+                    IdentifierName("Shared")))
+        ]);
+    }
+
+    private static List<StatementSyntax> GetRegisterBlock(IEnumerable<string> packetTypes)
+    {
+        var result = new List<StatementSyntax>();
+
+        foreach (var type in packetTypes)
+        {
+            var statement = ExpressionStatement(
+                InvocationExpression(
+                    MemberAccessExpression(
+                        SyntaxKind
+                            .SimpleMemberAccessExpression,
+                        IdentifierName(
+                            "options"),
+                        GenericName(
+                                Identifier(
+                                    "Register"))
+                            .WithTypeArgumentList(
+                                TypeArgumentList(SingletonSeparatedList<TypeSyntax>(IdentifierName(type)))))));
+
+            result.Add(statement);
+        }
+
+        return result;
+    }
+
+    private static SyntaxList<MemberDeclarationSyntax> GetMethodBody(IEnumerable<string> packetTypes, string assemblyName)
+    {
+        return SingletonList<MemberDeclarationSyntax>(
+            GlobalStatement(
+                LocalFunctionStatement(
+                        IdentifierName("IServiceCollection"),
+                        Identifier($"Register{assemblyName.Replace(".", string.Empty)}Packets"))
+                    .WithModifiers(
+                        TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.StaticKeyword)))
+                    .WithParameterList(
+                        ParameterList(
+                            SingletonSeparatedList(
+                                Parameter(
+                                        Identifier("services"))
+                                    .WithModifiers(
+                                        TokenList(
+                                            Token(SyntaxKind.ThisKeyword)))
+                                    .WithType(
+                                        IdentifierName("IServiceCollection")))))
+                    .WithBody(
+                        Block(
+                            ExpressionStatement(
+                                InvocationExpression(
+                                        MemberAccessExpression(
+                                            SyntaxKind.SimpleMemberAccessExpression,
+                                            IdentifierName("services"),
+                                            GenericName(
+                                                    Identifier("Configure"))
+                                                .WithTypeArgumentList(
+                                                    TypeArgumentList(
+                                                        SingletonSeparatedList<TypeSyntax>(
+                                                            IdentifierName("PacketIdMapperOptions"))))))
+                                    .WithArgumentList(
+                                        ArgumentList(
+                                            SingletonSeparatedList(
+                                                Argument(
+                                                    SimpleLambdaExpression(
+                                                            Parameter(
+                                                                Identifier("options")))
+                                                        .WithBlock(
+                                                            Block(GetRegisterBlock(packetTypes)
+                                                                .ToArray()))))))),
+                            ReturnStatement(
+                                IdentifierName("services"))
+                        ))));
+    }
+
+    private static SyntaxList<MemberDeclarationSyntax> GetClassDecl(IEnumerable<string> packetTypes, string assemblyName)
+    {
+        return SingletonList<MemberDeclarationSyntax>(
+            FileScopedNamespaceDeclaration(
+                    IdentifierName(assemblyName))
+                .WithMembers(
+                    SingletonList<MemberDeclarationSyntax>(
+                        ClassDeclaration($"{assemblyName.Replace(".", string.Empty)}PacketRegisterHelper")
+                            .WithModifiers(
+                                TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.StaticKeyword)))
+                            .WithMembers(GetMethodBody(packetTypes, assemblyName)))));
+    }
+
+    public static CompilationUnitSyntax GetCompleteDecl(IEnumerable<string> packetTypes, string assemblyName)
+    {
+        return CompilationUnit()
+            .WithUsings(GetUsings())
+            .WithMembers(GetClassDecl(packetTypes, assemblyName));
+    }
+}
